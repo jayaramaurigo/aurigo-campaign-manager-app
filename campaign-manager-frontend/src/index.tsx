439cbc3d92c303e180c43048d82b174aaa433b53
V-Jayaram J <v-jayaram@Aurigo.com>
2023-03-14 14:26:58 +0530

    Initial commit
    react front-end added

---
+++ campaign-manager-frontend/src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Homepage from './components/homepage';
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  
+ <Homepage/>
+ 
+);
+
