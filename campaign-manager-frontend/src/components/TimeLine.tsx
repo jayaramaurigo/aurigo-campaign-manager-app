2bdd2bbc776556199682c60f1abdab6df3834bcb
VenomCJ7 <vishwasbaya@gmail.com>
2023-03-22 14:18:31 +0530

    Just regular commit

--- campaign-manager-frontend/src/components/TimeLine.tsx
+++ campaign-manager-frontend/src/components/TimeLine.tsx
@@ -29,66 +29,66 @@ export default function CustomizedTimeLine() {
             },
         }}
     >
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot>
-            <FastfoodIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Eat
-          </Typography>
-          <Typography>Because you need strength</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary">
-            <LaptopMacIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Code
-          </Typography>
-          <Typography>Because it&apos;s awesome!</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector />
-          <TimelineDot color="primary" variant="outlined">
-            <HotelIcon />
-          </TimelineDot>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Sleep
-          </Typography>
-          <Typography>Because you need rest</Typography>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-          <TimelineDot color="secondary">
-            <RepeatIcon />
-          </TimelineDot>
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent sx={{ py: '12px', px: 2 }}>
-          <Typography variant="h6" component="span">
-            Repeat
-          </Typography>
-          <Typography>Because this is the life you love!</Typography>
-        </TimelineContent>
-      </TimelineItem>
+        <TimelineItem>
+            <TimelineSeparator>
+                <TimelineConnector />
+                    <TimelineDot>
+                        <FastfoodIcon />
+                    </TimelineDot>
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h6" component="span">
+                    Eat
+                </Typography>
+                <Typography>Because you need strength</Typography>
+            </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+            <TimelineSeparator>
+                <TimelineConnector />
+                    <TimelineDot color="primary">
+                        <LaptopMacIcon />
+                    </TimelineDot>
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h6" component="span">
+                    Code
+                </Typography>
+                <Typography>Because it&apos;s awesome!</Typography>
+            </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+            <TimelineSeparator>
+                <TimelineConnector />
+                    <TimelineDot color="primary" variant="outlined">
+                        <HotelIcon />
+                    </TimelineDot>
+            <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h6" component="span">
+                    Sleep
+                </Typography>
+                <Typography>Because you need rest</Typography>
+            </TimelineContent>
+        </TimelineItem>
+        <TimelineItem>
+            <TimelineSeparator>
+                <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                    <TimelineDot color="secondary">
+                        <RepeatIcon />
+                    </TimelineDot>
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+                <Typography variant="h6" component="span">
+                    Repeat
+                </Typography>
+                <Typography>Because this is the life you love!</Typography>
+            </TimelineContent>
+        </TimelineItem>
     </Timeline>
   );
 }
